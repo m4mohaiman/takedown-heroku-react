17fa2fbe74a66832b99bd8c640f1083b35d792ab
moh.dev <mmohaiman@paradynamix.com>
2020-02-18 17:53:34 +0600

    first commit

---
+++ build/precache-manifest.61f830205e24a305d3eca875650b2516.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286f1c45eb3f033cb7cedd47e263d0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd763efec622564509d3",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "652ebd3078fa08ec2e4e",
+    "url": "/static/css/main.604bf955.chunk.css"
+  },
+  {
+    "revision": "fd763efec622564509d3",
+    "url": "/static/js/2.22a924e4.chunk.js"
+  },
+  {
+    "revision": "0f3c990d3332ea672c55d229dbc2e342",
+    "url": "/static/js/2.22a924e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652ebd3078fa08ec2e4e",
+    "url": "/static/js/main.58ef5820.chunk.js"
+  },
+  {
+    "revision": "e25d88acb794601fba3d",
+    "url": "/static/js/runtime-main.6b9bface.js"
+  },
+  {
+    "revision": "d8cee790518e98bf074698b44f26e5e5",
+    "url": "/static/media/takedoenlogo.d8cee790.png"
+  },
+  {
+    "revision": "6529b486915b0c6e98d12f429fed1454",
+    "url": "/static/media/takedown-login-bg.6529b486.jpg"
+  }
+]);
